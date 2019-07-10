0d7c360351f88e42dae4d99cc8933ade0373e66d
Toby Yan <me@tobyan.com>
2019-07-10 15:31:32 +0800

    update alert str
    Signed-off-by: Toby Yan <me@tobyan.com>

--- app/logics/alert/notifiers/pushover.go
+++ app/logics/alert/notifiers/pushover.go
@@ -14,13 +14,20 @@ type Pushover struct {
 
 }
 func (po *Pushover)Notify(pair string, direction alert.Direction, differencePercentage string, price string, rawDataStr string) error {
+	directionStr := "-"
+	if direction == alert.Up {
+		directionStr = "📈"
+	}else{
+		directionStr = "️📉"
+	}
+
 	_, err := biu.Ready(biu.MethodPost, PushOverMessageUrl, &biu.Options{
 		Body: &biu.Body{
 			"token":   config.GetString("pushover.token"),
 			"user":    config.GetString("pushover.user"),
 			"device":  config.GetString("pushover.device"),
 
-			"title":   fmt.Sprintf("%s %s %s !!! now price: %s", pair,  direction, differencePercentage, price),
+			"title":   fmt.Sprintf("[%s] %s %s !!! {%s}", pair,  directionStr, differencePercentage, price),
 			"message": string(rawDataStr),
 		},
 	}).Biu().Status()
